6f064f912ef39ff9f80d95eefeda06f1bfc84387
Abdulhaseeb12-99 <abdulhaseebsultanpuri@gmail.com>
2025-05-26 00:00:55 +0530

    Update load_data.py

---
+++ src/load_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+def load_data(file_path):
+    try:
+        df = pd.read_csv(file_path)
+        print("✅ Data loaded successfully.")
+        return df
+    except Exception as e:
+        print(f"❌ Error loading data: {e}")
+        return None
+
+def clean_data(df):
+    df.columns = df.columns.str.strip()
+    print("Missing values:\n", df.isnull().sum())
+    df_cleaned = df.dropna()
+    print("✅ Data cleaned. Shape:", df_cleaned.shape)
+    return df_cleaned
+
+# Test code (you can remove this part before pushing)
+if __name__ == "__main__":
+    data = load_data("data/50_Startups.csv")
+    if data is not None:
+        clean_data(data)
