d60b36d7a3c7d7e3a9ba8146050025dde81c1588
Shaikyusuf1717 <shaikyusuf1717@gmail.com>
2025-05-26 00:36:36 +0530

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -2,17 +2,17 @@ import pandas as pd
 def load_data(file_path):
     try:
         df = pd.read_csv(file_path)
-        print("✅ Data loaded successfully.")
+        print("Data loaded successfully.")
         return df
     except Exception as e:
-        print(f"❌ Error loading data: {e}")
+        print(f"Error loading data: {e}")
         return None
 
 def clean_data(df):
     df.columns = df.columns.str.strip()
     print("Missing values:\n", df.isnull().sum())
     df_cleaned = df.dropna()
-    print("✅ Data cleaned. Shape:", df_cleaned.shape)
+    print("Data cleaned. Shape:", df_cleaned.shape)
     return df_cleaned
 
 if __name__ == "__main__":
